5f40dcd1859b858f9d2dfaa4089e5ad722003464
Marcel Stadelmann <m@marceli.to>
2023-06-21 13:56:32 +0200

    wip

--- resources/views/pages/office/team.blade.php
+++ resources/views/pages/office/team.blade.php
@@ -3,117 +3,121 @@
 <x-page-title />
 <section class="content-teams">
 
-  <article class="team">
-    <h2>{{ isset($data['bsa_team']->name) }}</h2>
-  </article>
-  <div class="lg:grid lg:grid-cols-12 lg:grid-column-gap">
-    <div class="lg:span-6">
-      @if (isset($data['bsa_team']->publishedImages[0]))
-        <div class="mb-20x lg:mb-50x">
+  @if (isset($data['bsa_team']->name))
+    <article class="team">
+      <h2>{{ $data['bsa_team']->name }}</h2>
+    </article>
+    <div class="lg:grid lg:grid-cols-12 lg:grid-column-gap">
+      <div class="lg:span-6">
+        @if (isset($data['bsa_team']->publishedImages[0]))
+          <div class="mb-20x lg:mb-50x">
+            <x-image 
+              :maxSizes="[0 => 1200, 1000 => 1600]" 
+              :image="$data['bsa_team']->publishedImages[0]" 
+              width="1200" 
+              height="800">
+            </x-image>
+          </div>
+        @endif
+        @if (isset($data['bsa_leadership']))
+          <h3>{{ $data['bsa_leadership']['category']->name}}</h3>
+        @endif
+        @if (isset($data['bsa_leadership']['employees']))
+          @foreach($data['bsa_leadership']['employees'] as $employee)
+            <div class="team__member">
+              <strong>{{ $employee->firstname}} {{ $employee->name}}</strong><br>
+              {{ $employee->title}}
+            </div>
+          @endforeach
+        @endif
+      </div>
+      <div class="lg:span-6">
+        @if (isset($data['bsa_team']->publishedImages[1]))
+          <div class="mb-20x lg:mb-50x">
+            <x-image 
+              :maxSizes="[0 => 1200, 1000 => 1600]" 
+              :image="$data['bsa_team']->publishedImages[1]" 
+              width="1200" 
+              height="800">
+            </x-image>
+          </div>
+        @endif
+        @if (isset($data['bsa_employees']))
+          <h3>{{ $data['bsa_employees']['category']->name}}</h3>
+        @endif
+        @if (isset($data['bsa_employees']['employees']))
+          <div class="lg:grid lg:grid-cols-12 lg:grid-column-gap">
+            @foreach($data['bsa_employees']['employees'] as $employees)
+              <div class="lg:span-6">
+                @foreach($employees as $employee)
+                  <div class="team__member">
+                    <strong>{{ $employee->firstname}} {{ $employee->name}}</strong><br>
+                    {{ $employee->title}}
+                  </div>
+                @endforeach
+              </div>
+            @endforeach
+          </div>
+        @endif
+      </div>
+    </div>
+  @endif
+  
+  @if (isset($data['bsemi_team']->name))
+    <article class="team mt-80x">
+      <h2>{{ $data['bsemi_team']->name }}</h2>
+    </article>
+    <div class="lg:grid lg:grid-cols-12 lg:grid-column-gap">
+      <div class="lg:span-6">
+        @if (isset($data['bsemi_team']->publishedImages[0]))
           <x-image 
             :maxSizes="[0 => 1200, 1000 => 1600]" 
-            :image="$data['bsa_team']->publishedImages[0]" 
+            :image="$data['bsemi_team']->publishedImages[0]" 
             width="1200" 
             height="800">
           </x-image>
-        </div>
-      @endif
-      @if (isset($data['bsa_leadership']))
-        <h3>{{ $data['bsa_leadership']['category']->name}}</h3>
-      @endif
-      @if (isset($data['bsa_leadership']['employees']))
-        @foreach($data['bsa_leadership']['employees'] as $employee)
-          <div class="team__member">
-            <strong>{{ $employee->firstname}} {{ $employee->name}}</strong><br>
-            {{ $employee->title}}
-          </div>
-        @endforeach
-      @endif
-    </div>
-    <div class="lg:span-6">
-      @if (isset($data['bsa_team']->publishedImages[1]))
-        <div class="mb-20x lg:mb-50x">
+        @endif
+        @if (isset($data['bsemi_leadership']))
+          <h3>{{ $data['bsemi_leadership']['category']->name}}</h3>
+        @endif
+        @if (isset($data['bsemi_leadership']['employees']))
+          @foreach($data['bsemi_leadership']['employees'] as $employee)
+            <div class="team__member">
+              <strong>{{ $employee->firstname}} {{ $employee->name}}</strong><br>
+              {{ $employee->title}}
+            </div>
+          @endforeach
+        @endif
+      </div>
+      <div class="lg:span-6">
+        @if (isset($data['bsemi_team']->publishedImages[1]))
           <x-image 
             :maxSizes="[0 => 1200, 1000 => 1600]" 
-            :image="$data['bsa_team']->publishedImages[1]" 
+            :image="$data['bsemi_team']->publishedImages[1]" 
             width="1200" 
             height="800">
           </x-image>
-        </div>
-      @endif
-      @if (isset($data['bsa_employees']))
-        <h3>{{ $data['bsa_employees']['category']->name}}</h3>
-      @endif
-      @if (isset($data['bsa_employees']['employees']))
-        <div class="lg:grid lg:grid-cols-12 lg:grid-column-gap">
-          @foreach($data['bsa_employees']['employees'] as $employees)
-            <div class="lg:span-6">
-              @foreach($employees as $employee)
-                <div class="team__member">
-                  <strong>{{ $employee->firstname}} {{ $employee->name}}</strong><br>
-                  {{ $employee->title}}
-                </div>
-              @endforeach
-            </div>
-          @endforeach
-        </div>
-      @endif
-    </div>
-  </div>
-
-  <article class="team mt-80x">
-    <h2>{{ isset($data['bsemi_team']) && $data['bsemi_team']->name) }}</h2>
-  </article>
-  <div class="lg:grid lg:grid-cols-12 lg:grid-column-gap">
-    <div class="lg:span-6">
-      @if (isset($data['bsemi_team']->publishedImages[0]))
-        <x-image 
-          :maxSizes="[0 => 1200, 1000 => 1600]" 
-          :image="$data['bsemi_team']->publishedImages[0]" 
-          width="1200" 
-          height="800">
-        </x-image>
-      @endif
-      @if (isset($data['bsemi_leadership']))
-        <h3>{{ $data['bsemi_leadership']['category']->name}}</h3>
-      @endif
-      @if (isset($data['bsemi_leadership']['employees']))
-        @foreach($data['bsemi_leadership']['employees'] as $employee)
-          <div class="team__member">
-            <strong>{{ $employee->firstname}} {{ $employee->name}}</strong><br>
-            {{ $employee->title}}
+        @endif
+        @if (isset($data['bsemi_employees']))
+          <h3>{{ $data['bsemi_employees']['category']->name}}</h3>
+        @endif
+        @if (isset($data['bsemi_employees']['employees']))
+          <div class="lg:grid lg:grid-cols-12 lg:grid-column-gap">
+            @foreach($data['bsemi_employees']['employees'] as $employees)
+              <div class="lg:span-6">
+                @foreach($employees as $employee)
+                  <div class="team__member">
+                    <strong>{{ $employee->firstname}} {{ $employee->name}}</strong><br>
+                    {{ $employee->title}}
+                  </div>
+                @endforeach
+              </div>
+            @endforeach
           </div>
-        @endforeach
-      @endif
+        @endif
+      </div>
     </div>
-    <div class="lg:span-6">
-      @if (isset($data['bsemi_team']->publishedImages[1]))
-        <x-image 
-          :maxSizes="[0 => 1200, 1000 => 1600]" 
-          :image="$data['bsemi_team']->publishedImages[1]" 
-          width="1200" 
-          height="800">
-        </x-image>
-      @endif
-      @if (isset($data['bsemi_employees']))
-        <h3>{{ $data['bsemi_employees']['category']->name}}</h3>
-      @endif
-      @if (isset($data['bsemi_employees']['employees']))
-        <div class="lg:grid lg:grid-cols-12 lg:grid-column-gap">
-          @foreach($data['bsemi_employees']['employees'] as $employees)
-            <div class="lg:span-6">
-              @foreach($employees as $employee)
-                <div class="team__member">
-                  <strong>{{ $employee->firstname}} {{ $employee->name}}</strong><br>
-                  {{ $employee->title}}
-                </div>
-              @endforeach
-            </div>
-          @endforeach
-        </div>
-      @endif
-    </div>
-  </div>
+  @endif
 
   <article class="team mt-80x">
     <h2>{{ $data['former_employees']['category']->name }}</h2>
