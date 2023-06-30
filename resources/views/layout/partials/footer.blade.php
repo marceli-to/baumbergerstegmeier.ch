36e1f47b34c87096aef3b576f904abd44d55b857
Marcel Stadelmann <m@marceli.to>
2023-06-30 21:39:46 +0200

    wip

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="site-footer">
   <div>
     Baumberger Stegmeier Architektur AG BSA SIA<br>
-    Weststrasse 70, CH-8004 Zürich<br>
+    Weststrasse 70, CH-8003 Zürich<br>
     <a href="mailto:info@baumbergerstegmeier.ch" title="E-mail">info@baumbergerstegmeier.ch</a>, 
     <a href="tel:+41 43 500 54 00" title="Telefon">+41 43 500 54 00</a>
     <div class="mt-10x lg:mt-15x">
