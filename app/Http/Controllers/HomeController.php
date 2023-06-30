f78296592e03a3416e534b8f0462ea153446eabc
Marcel Stadelmann <m@marceli.to>
2023-06-30 16:50:21 +0200

    wip

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -53,7 +53,7 @@ class HomeController extends BaseController
     $query = Teaser::publish()->with('image', 'project', 'article.publishedImage')->where('type', 'home');
     $items = $query->orderBy('position')->get();
 
-    dd($items);
+    dd($items, $items->groupBy('column')->values());
 
     return $items->groupBy('column')->values();
   }
