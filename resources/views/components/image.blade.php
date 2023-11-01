8f2702761db623e16457aa32166d1983ce047916
Marcel Stadelmann <m@marceli.to>
2023-11-01 21:45:07 +0100

    wip

--- resources/views/components/image.blade.php
+++ resources/views/components/image.blade.php
@@ -5,8 +5,7 @@
       @if ($minWidth > 0)
         <source media="(min-width: {{ $minWidth }}px)" data-srcset="/img/cache/{{ $image->name }}/{{ $maxSize}}/{{ $image->coords }}{{ $ratio ? '/' . $ratio : ''}}">
       @endif
-      @endforeach
-
+    @endforeach
     <img 
     src="/img/cache/{{ $image->name }}/{{ $maxSize }}/{{ $image->coords }}{{ $ratio ? '/' . $ratio : ''}}"
     width="{{ $width }}" 
