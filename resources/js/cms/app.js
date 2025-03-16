24dc4348e2eeec36af2a728d99361b2455f99213
Marcel Stadelmann <m@marceli.to>
2025-03-16 10:11:07 +0100

    upgraded node_modules

--- resources/js/cms/app.js
+++ resources/js/cms/app.js
@@ -1,38 +1,39 @@
 require('@/bootstrap');
 
 // Vue
-window.Vue = require('vue');
-
-// Axios Interceptors
-require('vue-axios-interceptors');
+import * as Vue from 'vue';
+window.Vue = Vue.default;
 
 // Axios, Vue-Axios
 import VueAxios from 'vue-axios';
 import axios from 'axios';
 window.axios = require('axios');
-Vue.use(VueAxios, axios);
+window.Vue.use(VueAxios, axios);
+
+// Axios Interceptors
+window.Vue.use(require('vue-axios-interceptors'));
 
 // Filters
 require('@/mixins/Filters');
 
 // Vue-Axios defaults
-Vue.axios.defaults.withCredentials = true;
+window.Vue.axios.defaults.withCredentials = true;
 
 // Vue-Notifications
 import Notifications from 'vue-notification';
-Vue.use(Notifications);
+window.Vue.use(Notifications);
 
 // Vue-Router
 import VueRouter from 'vue-router';
-Vue.use(VueRouter);
+window.Vue.use(VueRouter);
 
 // Loading indicator
 import LoadingIndicator from "@/components/ui/LoadingIndicator";
-Vue.component('LoadingIndicator', LoadingIndicator);
+window.Vue.component('LoadingIndicator', LoadingIndicator);
 
 // Separator
 import Separator from "@/components/ui/Separator";
-Vue.component('Separator', Separator);
+window.Vue.component('Separator', Separator);
 
 // Store
 import store from '@/config/store';
@@ -71,7 +72,7 @@ const router = new VueRouter(
 import AppComponent from '@/App.vue';
 
 // Mount App
-const app = new Vue({
+const app = new window.Vue({
   mixins: [],
   components: { 
     AppComponent
