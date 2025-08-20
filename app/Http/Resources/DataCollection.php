8f5194cb2729549d24004766237f2b3360e85e8a
Marcel Stadelmann <m@marceli.to>
2025-08-20 13:21:11 +0200

    fixes invalid utf8 data in awards

--- app/Http/Resources/DataCollection.php
+++ app/Http/Resources/DataCollection.php
@@ -12,6 +12,21 @@ class DataCollection extends ResourceCollection
    */
   public function toArray($request)
   {
-    return parent::toArray($request);
+    $data = parent::toArray($request);
+    
+    return $this->sanitizeUtf8($data);
+  }
+
+  private function sanitizeUtf8($data)
+  {
+    if (is_array($data)) {
+      foreach ($data as $key => $value) {
+        $data[$key] = $this->sanitizeUtf8($value);
+      }
+    } elseif (is_string($data)) {
+      $data = mb_convert_encoding($data, 'UTF-8', 'UTF-8');
+    }
+    
+    return $data;
   }
 }
